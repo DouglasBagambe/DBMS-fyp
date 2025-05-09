3dd75713287326f12bf913d5cb1b5f6138bb63e0
douglasbagambe <douglasbagambe4@gmail.com>
2025-05-09 22:56:19 +0300

    permiting localhost 1 and 2

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ app.use(helmet());
 app.use(compression());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "*", "http://localhost:3000"],
+    origin: [
+      process.env.FRONTEND_URL || "*",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:3002",
+    ],
     credentials: true,
   })
 );
