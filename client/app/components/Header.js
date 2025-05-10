18c07380ee5a35e457a200a29232c342c1053a10
douglasbagambe <douglasbagambe4@gmail.com>
2025-05-10 13:45:58 +0300

    document error

--- client/app/components/Header.js
+++ client/app/components/Header.js
@@ -30,10 +30,12 @@ const Header = () => {
         setShowMobileMenu(false);
       }
     };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
+    if (typeof document !== "undefined") {
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }
   }, []);
 
   // Toggle menus
